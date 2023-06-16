06d9ca5a00b18bc126e58b40467f1b4283cbe57e
krivalex <new.krivalex@gmail.com>
2023-06-16 22:49:05 +0600

    pin

--- src/pages/NotFoundPage.vue
+++ src/pages/NotFoundPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="formula-css">
-    <img src="@/assets/pin.png">
+    <img class="pin" src="@/assets/pin.png">
   </div>
 </template>
 
@@ -30,8 +30,33 @@ export default {
   justify-content: center;
   margin: 0 auto;
   margin-top: 50px;
-  border: 20px solid #5a3a02;
+  border: 15px solid #502d16;
   border-top: none;
-  border-radius: 30px;
+  border-bottom-left-radius: 10%;
+  border-bottom-right-radius: 10%;
+  box-shadow: 5px 2px 0px 1px #0a0a0a;
+}
+
+.pin {
+  width: 50px;
+  height: 50px;
+  position: absolute;
+  top: -20px;
+  right: 10px;
+  animation: pin 1s infinite;
+}
+
+@keyframes pin {
+  0% {
+    transform: rotate(0deg);
+  }
+
+  50% {
+    transform: rotate(10deg);
+  }
+
+  100% {
+    transform: rotate(0deg);
+  }
 }
 </style>
