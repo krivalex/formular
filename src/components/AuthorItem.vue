d41dfe55506d8b1716f360a2a09d97e6372cf8e6
krivalex <new.krivalex@gmail.com>
2023-06-17 19:55:32 +0600

    author update

--- src/components/AuthorItem.vue
+++ src/components/AuthorItem.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="author-item">
-
+    <img class="texture" src="@/assets/background/paper-text-2.jpg">
+    <div class="content">
+      <img :src="author.photoURL" alt="author avatar" />
+      <p>{{ author.displayName }}</p>
+    </div>
   </div>
 </template>
 
@@ -16,3 +20,46 @@ export default {
 }
 
 </script>
+
+<style lang="scss" scoped>
+@import "@/assets/style.scss";
+
+.author-item {
+  @include adaptiv-font(18, 12);
+  @include adaptiv-width(400, 300);
+  @include adaptiv-height(100, 50);
+  z-index: 100;
+  position: relative;
+  background-color: #f1f7c1;
+  margin: 0 auto;
+  margin-top: 50px;
+  border: 15px solid #502d16;
+  border-bottom: none;
+  border-top-left-radius: 10%;
+  border-top-right-radius: 10%;
+  box-shadow: 5px 2px 0px 1px #0a0a0a;
+
+  img {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    margin-right: 10px;
+  }
+
+  p {
+    @include adaptiv-font(18, 12);
+    font-weight: 700;
+  }
+
+  .texture {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    opacity: 0.5;
+    border-radius: 0%;
+  }
+}
+</style>
--- src/components/FormulaItem.vue
+++ src/components/FormulaItem.vue
@@ -1,15 +1,12 @@
 <template>
+  <author-item :author="formula.author" />
   <div class="formula-css">
     <img class="texture" src="@/assets/background/paper-text.jpg">
     <div class="content">
+
       <h1>{{ formula.name }}</h1>
+
       <img class="main-image" :src="formula.image_link" alt="formula image" />
-      <div class="difficulty">
-        <star-rating :star-size="20" :show-rating="false" :read-only="true" :rating="formula.difficulty"
-          :active-color="['#a1eb34', '#ebd934', '#eb9f34', '#eb5f34', '#eb3d34']" :active-border-color="['#a8c3c0']"
-          :border-width="4"
-          :star-points="[23, 2, 14, 17, 0, 19, 10, 34, 7, 50, 23, 43, 38, 50, 36, 34, 46, 19, 31, 17]" />
-      </div>
 
       <div v-for="aspect in formula.aspects" :key="aspect.stroke" class="row">
         <div class="strokes">
@@ -20,21 +17,35 @@
       </div>
 
       <p class="formula-description">{{ formula.description }}</p>
-      <p>{{ formula.category }}</p>
-      <p>{{ formula.kitchen }}</p>
-      <p>{{ formula.author.displayName }}</p>
-      <p>{{ dateFormatter }}</p>
+
+      <div class="category-kitchen">
+        <p>{{ formula.category }}</p>
+        <p>{{ formula.kitchen }}</p>
+      </div>
+
+      <div class="diffuculty-date">
+        <div class="difficulty">
+          <star-rating :star-size="20" :show-rating="false" :read-only="true" :rating="formula.difficulty"
+            :active-color="['#a1eb34', '#ebd934', '#eb9f34', '#eb5f34', '#eb3d34']" :active-border-color="['#a8c3c0']"
+            :border-width="4"
+            :star-points="[23, 2, 14, 17, 0, 19, 10, 34, 7, 50, 23, 43, 38, 50, 36, 34, 46, 19, 31, 17]" />
+        </div>
+        <p>{{ dateFormatter }}</p>
+      </div>
+
     </div>
   </div>
 </template>
 
 <script>
 import StarRating from 'vue-star-rating';
+import AuthorItem from './AuthorItem.vue';
 
 export default {
   name: "formula-item",
   components: {
     StarRating,
+    AuthorItem,
   },
   props: {
     formula: {
@@ -77,12 +88,25 @@ export default {
   align-items: center;
   justify-content: center;
   margin: 0 auto;
-  margin-top: 50px;
   border: 15px solid #502d16;
-  border-top: 7px solid #eaf3a3;
+  border-top: none;
   border-bottom-left-radius: 10%;
   border-bottom-right-radius: 10%;
   box-shadow: 5px 2px 0px 1px #0a0a0a;
+
+  .author-title {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+
+    h1 {
+      @include adaptiv-font(24, 16);
+      font-weight: 700;
+      margin: 0;
+    }
+  }
 
   .divider {
     height: 2px;
@@ -107,7 +131,7 @@ export default {
     top: 0;
     left: 0;
     z-index: -1;
-    opacity: 0.3;
+    opacity: 0.4;
   }
 
   &::before {
@@ -118,7 +142,7 @@ export default {
     width: 100%;
     height: 4px;
     background-color: gray;
-    outline: 2px dashed gray;
+    outline: 4px dashed gray;
     margin: 0 auto;
     box-shadow: 0px 0px 0px 1px #0a0a0a;
   }
