80e84805374c1e971bda47f32753f09fe754df6c
krivalex <new.krivalex@gmail.com>
2023-06-17 19:02:50 +0600

    formula update

---
+++ src/components/AuthorItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="author-item">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "author-item",
+  props: {
+    author: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+
+</script>
--- src/components/FormulaItem.vue
+++ src/components/FormulaItem.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="formula-css">
-    <img class="pin" src="@/assets/pin.png">
+    <img class="texture" src="@/assets/background/paper-text.jpg">
     <div class="content">
       <h1>{{ formula.name }}</h1>
       <img class="main-image" :src="formula.image_link" alt="formula image" />
-      <div class="row difficulty">
-        <p>Сложность: </p>
+      <div class="difficulty">
         <star-rating :star-size="20" :show-rating="false" :read-only="true" :rating="formula.difficulty"
           :active-color="['#a1eb34', '#ebd934', '#eb9f34', '#eb5f34', '#eb3d34']" :active-border-color="['#a8c3c0']"
           :border-width="4"
@@ -13,15 +12,18 @@
       </div>
 
       <div v-for="aspect in formula.aspects" :key="aspect.stroke" class="row">
-        <p class="stroke">{{ aspect.stroke }}</p>
-        <p class="stroke">{{ aspect.name }}</p>
+        <div class="strokes">
+          <p class="stroke">{{ aspect.stroke }}. {{ aspect.aspect }}</p>
+          <p class="stroke">{{ aspect.count }} {{ aspect.unit }}</p>
+        </div>
         <div class="divider"></div>
       </div>
+
       <p class="formula-description">{{ formula.description }}</p>
       <p>{{ formula.category }}</p>
       <p>{{ formula.kitchen }}</p>
       <p>{{ formula.author.displayName }}</p>
-      <p>{{ formula.date }}</p>
+      <p>{{ dateFormatter }}</p>
     </div>
   </div>
 </template>
@@ -43,6 +45,18 @@ export default {
   created() {
     console.log(this.formula);
   },
+  computed: {
+    dateFormatter() {
+      const date = this.formula.date
+      let result_date = new Date(date.slice(0, 4), Number(date.slice(5, 7)) - 1, date.slice(8, 10));
+      let formatter = new Intl.DateTimeFormat("ru", {
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+      });
+      return formatter.format(result_date);
+    },
+  }
 }
 
 
@@ -52,7 +66,7 @@ export default {
 @import "@/assets/style.scss";
 
 .formula-css {
-  @include adaptiv-font(20, 12);
+  @include adaptiv-font(18, 12);
   @include adaptiv-width(400, 300);
   @include adaptiv-height(500, 400);
   z-index: 100;
@@ -65,47 +79,61 @@ export default {
   margin: 0 auto;
   margin-top: 50px;
   border: 15px solid #502d16;
-  border-top: none;
+  border-top: 7px solid #eaf3a3;
   border-bottom-left-radius: 10%;
   border-bottom-right-radius: 10%;
   box-shadow: 5px 2px 0px 1px #0a0a0a;
 
   .divider {
-    @include adaptiv-width(320, 250);
     height: 2px;
     background-color: black;
   }
 
   .main-image {
-    @include adaptiv-width(320, 250);
-    @include adaptiv-height(220, 250);
+    @include adaptiv-width(340, 240);
+    @include adaptiv-height(150, 80);
     object-fit: cover;
     margin: 0 auto;
-    margin-top: 20px;
-    margin-bottom: 20px;
+    border: 2px solid #502d16;
+    box-shadow: 3px 2px 0px 1px #0a0a0a;
+  }
+
+  .texture {
+    width: 100%;
+    height: 100%;
+    border-bottom-left-radius: 10%;
+    border-bottom-right-radius: 10%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    opacity: 0.3;
+  }
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 4px;
+    background-color: gray;
+    outline: 2px dashed gray;
+    margin: 0 auto;
+    box-shadow: 0px 0px 0px 1px #0a0a0a;
   }
 }
 
-.pin {
-  width: 50px;
-  height: 50px;
-  position: absolute;
-  top: -20px;
-  right: 10px;
-  animation: pin 1s infinite;
-}
+.strokes {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 10px;
 
-@keyframes pin {
-  0% {
-    transform: rotate(0deg);
-  }
-
-  50% {
-    transform: rotate(10deg);
-  }
-
-  100% {
-    transform: rotate(0deg);
+  .stroke {
+    display: flex;
+    justify-content: space-between;
   }
 }
 </style>
